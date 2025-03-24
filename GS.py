07c6c23334f923c111d3c34483510f11578fd08d
JaroslavJanovsky <jaroslav.janovsky1994@seznam.cz>
2025-03-24 11:41:48 +0100

    Delete GS.py

--- GS.py
+++
@@ -1,8 +0,0 @@
-def hi(name):
-     print('No nazdar ' + name + '!')
-
-kokoti = ['Fero', 'Lakatoš', 'Dežo', 'Bangladežo', 'Dežodorant']
-for name in kokoti:
-     hi(name)
-     print('další kokot')
-print('No nazdar poslední kokote!')
